644b8668ea06b9b0f9610e1f578a6f79f136799e
creditimpact <creditimpact1@gmail.com>
2025-08-01 17:48:34 -0400

    Expand questionnaire and dynamic docs

--- server/utils/caseStore.js
+++ server/utils/caseStore.js
@@ -3,11 +3,22 @@ const cases = {};
 function computeDocuments(answers = {}) {
   const docs = [
     { key: 'id_document', name: 'ID Document', uploaded: false, url: '' },
-    { key: 'financials', name: 'Financial Statements', uploaded: false, url: '' },
+    { key: 'tax_returns', name: 'Business Tax Returns', uploaded: false, url: '' },
+    { key: 'bank_statements', name: 'Bank Statements', uploaded: false, url: '' },
   ];
 
-  if (answers.businessType === 'Corporation') {
+  if (answers.businessType === 'Corporation' || answers.businessType === 'LLC') {
     docs.push({ key: 'incorporation_cert', name: 'Articles of Incorporation', uploaded: false, url: '' });
+    docs.push({ key: 'ein_proof', name: 'EIN Confirmation', uploaded: false, url: '' });
+  }
+
+  if (answers.businessType === 'Sole') {
+    docs.push({ key: 'business_license', name: 'Business License', uploaded: false, url: '' });
+    docs.push({ key: 'ssn_verification', name: 'Owner SSN', uploaded: false, url: '' });
+  }
+
+  if (answers.employees && Number(answers.employees) > 0) {
+    docs.push({ key: 'payroll_records', name: 'Payroll Records', uploaded: false, url: '' });
   }
 
   if (answers.cpaPrepared) {
