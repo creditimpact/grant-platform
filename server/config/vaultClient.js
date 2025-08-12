49b2d9b0b9759e8147a871d9beb795c61288e445
Agentaip <puremarketingglobal@gmail.com>
2025-08-12 13:49:53 -0600

    Save local changes before pull

--- server/config/vaultClient.js
+++ server/config/vaultClient.js
@@ -1,9 +1,15 @@
+// vaultClient.js
+
+import dotenv from 'dotenv';
 import Vault from 'node-vault';
 
+// טוען את משתני הסביבה מקובץ .env
+dotenv.config();
+
 const vault = Vault({
-  apiVersion: 'v1',
-  endpoint: process.env.VAULT_ADDR || 'http://127.0.0.1:8200',
-  token: process.env.VAULT_TOKEN, // הטוקן ייקרא מ־env variables
+  apiVersion: 'v1', // גרסת API של Vault
+  endpoint: process.env.VAULT_ADDR || 'http://127.0.0.1:8200', // כתובת Vault
+  token: process.env.VAULT_TOKEN, // טוקן לקריאה מ־env variables
 });
 
 export default vault;
